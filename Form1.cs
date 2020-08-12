b70e66fa469a239eed1fa221d57a4005cea391d2
Исмагилов Кирилл Флуньевич <lite@main.vil-kit.ru>
2020-08-12 17:11:17 +1200

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void таблицаСвойствToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            settings sets = new settings();
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            //string filename = ;
+            var l = sets.add_options(openFileDialog.FileName);
+        }
+    }
+}
+
+public partial  class settings
+{
+    List<string[]> options;
+
+    public settings() {
+        options = new List<string[]>();
+    }
+
+    public List<string[]> add_options(string file_name)
+    {
+        string fileText = System.IO.File.ReadAllText(file_name, Encoding.Default);
+
+        Regex get_line = new Regex("(.*)\r\n");
+        MatchCollection words = get_line.Matches(fileText);
+        Regex sub_string = new Regex(";");
+        string[] line;
+
+        int i = 0;
+        foreach (Match m in words)
+        {
+            if (i == 0) { i++; continue; }
+
+            line = sub_string.Split(m.Groups[1].Value);
+            string[] ops = new string[2];
+            if (line[0] != "")
+            {
+                ops[0] = line[0];
+                ops[1] = line[1];
+                options.Add(ops);
+            }
+        }
+
+        return options;
+    }
+}
