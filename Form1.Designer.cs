920b7d33f9c2998378a35ce753ee50e8b7542ad2
Исмагилов Кирилл Флуньевич <lite@main.vil-kit.ru>
2020-08-12 17:11:17 +1200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace stl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aфаилToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.таблицаСвойствToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aфаилToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aфаилToolStripMenuItem
+            // 
+            this.aфаилToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.таблицаСвойствToolStripMenuItem});
+            this.aфаилToolStripMenuItem.Name = "aфаилToolStripMenuItem";
+            this.aфаилToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.aфаилToolStripMenuItem.Text = "фаил";
+            // 
+            // таблицаСвойствToolStripMenuItem
+            // 
+            this.таблицаСвойствToolStripMenuItem.Name = "таблицаСвойствToolStripMenuItem";
+            this.таблицаСвойствToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.таблицаСвойствToolStripMenuItem.Text = "таблица свойств";
+            this.таблицаСвойствToolStripMenuItem.Click += new System.EventHandler(this.таблицаСвойствToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "Таблица свойств";
+            this.openFileDialog.Filter = "Таблица|*.csv|Все фаилы(*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aфаилToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem таблицаСвойствToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void таблицаСвойствToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            settings sets = new settings();
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            //string filename = ;
+            var l = sets.add_options(openFileDialog.FileName);
+            string[] files_opions = Directory.GetFiles("sl");
+
+            foreach (string file_option in files_opions)
+            {
+                string fileText = System.IO.File.ReadAllText(file_option, Encoding.Default);
+                Regex get_line = new Regex("(.*)\r\n");
+                Match words = get_line.Match(fileText);
+                Regex sub_string = new Regex(";");
+                
+                string []title = sub_string.Split(words.Value);
+
+                for (int i = 0; i < title.Length; i++)
+                    foreach (string[] it in l)
+                        if (title[i] == it[0])
+                            title[i] = it[1];
+            }
+        }
+    }
+}
+
+public partial  class settings
+{
+    List<string[]> options;
+
+    public settings() {
+        options = new List<string[]>();
+    }
+
+    public List<string[]> add_options(string file_name)
+    {
+        string fileText = System.IO.File.ReadAllText(file_name, Encoding.Default);
+
+        Regex get_line = new Regex("(.*)\r\n");
+        MatchCollection words = get_line.Matches(fileText);
+        Regex sub_string = new Regex(";");
+        string[] line;
+
+        int i = 0;
+        foreach (Match m in words)
+        {
+            if (i == 0) { i++; continue; }
+
+            line = sub_string.Split(m.Groups[1].Value);
+            string[] ops = new string[2];
+            if (line[0] != "")
+            {
+                ops[0] = line[0];
+                ops[1] = line[1];
+                options.Add(ops);
+            }
+        }
+
+        return options;
+    }
+}
