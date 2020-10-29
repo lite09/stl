b1777ba34dd973b4255f96ad0a5234531c495521
Кирилл Жизнь и Труд <klklss@yahoo.com>
2020-10-29 17:58:35 +1200

    i

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -80,7 +80,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(224, 82);
+            this.ClientSize = new System.Drawing.Size(224, 73);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
